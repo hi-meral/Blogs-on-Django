3a9cd467499b86a73773628de2d9e153d4cb7220
Meral Maradia <39996589+hi-meral@users.noreply.github.com>
2022-05-23 15:57:42 +0530

    Add files via direct upload

---
+++ blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.db.models.fields import CharField
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2080)
+
+    def __str__(self) -> str:
+        return self.name
